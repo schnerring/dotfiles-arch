3fd046c66d6e958127ce4177819fa09ae71dc2ae
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2020-04-08 00:31:37 +0200

    remove arguments from *.desktop files via sed

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-grep -rRh -m 1 ^Exec $XDG_CONFIG_DIRS/autostart | while read -r line ; do 
+grep -rRh -m 1 ^Exec $XDG_CONFIG_DIRS/autostart | sed -e 's/\(-- \)\?%[fFuU]//g' | while read -r line ; do 
    ${line:5} &
 done
 
-grep -rRh -m 1 ^Exec $XDG_CONFIG_HOME/autostart | while read -r line ; do 
+grep -rRh -m 1 ^Exec $XDG_CONFIG_HOME/autostart | sed -e 's/\(-- \)\?%[fFuU]//g' | while read -r line ; do 
    ${line:5} &
 done
