d76747190017b24181e9af0f5d6258b5c941343f
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2020-04-07 23:56:19 +0200

    i3 autostart: tune grep stmt

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-grep -rRh Exec $XDG_CONFIG_DIRS/autostart | while read -r line ; do 
+grep -rRh -m 1 ^Exec $XDG_CONFIG_DIRS/autostart | while read -r line ; do 
    ${line:5} &
 done
 
-grep -rRh Exec $XDG_CONFIG_HOME/autostart | while read -r line ; do 
+grep -rRh -m 1 ^Exec $XDG_CONFIG_HOME/autostart | while read -r line ; do 
    ${line:5} &
 done
