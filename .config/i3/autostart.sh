569ee33d32c2efa8a154e484e9d1941aece99327
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2020-04-07 23:41:45 +0200

    i3 autostart: dereference symbolic links

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-grep -rh Exec $XDG_CONFIG_DIRS/autostart | while read -r line ; do 
+grep -rRh Exec $XDG_CONFIG_DIRS/autostart | while read -r line ; do 
    ${line:5} &
 done
 
-grep -rh Exec $XDG_CONFIG_HOME/autostart | while read -r line ; do 
+grep -rRh Exec $XDG_CONFIG_HOME/autostart | while read -r line ; do 
    ${line:5} &
 done
